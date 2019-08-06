74a8a5a03cc7d6e7b525abdc13331e5c70d33fed
mastercad <andreas.kempe@byte-artist.de>
2019-08-06 08:22:34 +0200

    wrong docblock in staticTest fixed

--- tests/Service/StatisticTest.php
+++ tests/Service/StatisticTest.php
@@ -33,7 +33,8 @@ use Symfony\Component\Translation\TranslatorInterface;
  * @copyright  2018-2019 byte-artist
  * @license    http://www.php.net/license/3_01.txt  PHP License 3.01
  * @version    Release: @package_version@
- * @since  
+ * @since      File available since Release 1.0.0
+ */
 class StatisticTest extends WebTestCase
 {
     public function testStatisticGeneration()
